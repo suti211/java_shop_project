322620cf04f57e6fdac4e393935f3e90c0c0eac1
Bosnyák Imre Dávid <suti211@gmail.com>
2017-02-23 17:34:55 +0100

    implemented vonalkod, final fields, modified toString() in Tej class

--- shop_project/src/bolt/Tej.java
+++ shop_project/src/bolt/Tej.java
@@ -3,19 +3,28 @@ package bolt;
 import java.util.Date;
 
 public class Tej {
-
+	
+	private final int LITER = 10;
+	private final int FELLITER = 5;
+	private final int POHAR = 2;
+	
+	private final double ZSIROS = 2.8;
+	private final double FELZSIROS = 1.5;
+	
+	private long vonalKod;
 	private int urtartalom;
 	private String gyarto;
 	private Date szavatossagiIdo;
 	private double zsirtartalom;
 	private long ar;
 
-	public Tej(int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar) {
+	public Tej(long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom, long ar) {
 		this.urtartalom = urtartalom;
 		this.gyarto = gyarto;
 		this.szavatossagiIdo = szavatossagiIdo;
 		this.zsirtartalom = zsirtartalom;
 		this.ar = ar;
+		this.vonalKod = vonalKod;
 	}
 
 	public int getUrtartalom() {
@@ -34,6 +43,10 @@ public class Tej {
 		return zsirtartalom;
 	}
 
+	public long getVonalKod() {
+		return vonalKod;
+	}
+
 	public long getAr() {
 		return ar;
 	}
@@ -50,7 +63,7 @@ public class Tej {
 
 	@Override
 	public String toString() {
-		return "Tej [urtartalom=" + urtartalom + ", gyarto=" + gyarto + ", szavatossagiIdo=" + szavatossagiIdo
+		return "Tej [vonalkod=" + vonalKod + ", urtartalom=" + urtartalom + ", gyarto=" + gyarto + ", szavatossagiIdo=" + szavatossagiIdo
 				+ ", zsirtartalom=" + zsirtartalom + ", ar=" + ar + "]";
 	}
 	
