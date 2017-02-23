b0e54e2a57887563d6bb5cdaf33654d55cd0c86c
Bosnyák Imre Dávid <suti211@gmail.com>
2017-02-23 16:54:19 +0100

    Implemented Bolt class with method skeletons

---
+++ shop_project/src/bolt/Bolt.java
@@ -0,0 +1,42 @@
+package bolt;
+
+public class Bolt {
+	private String nev;
+	private String cim;
+	private String tulajdonos;
+	private Tej[] tejpult;
+	private int flag;
+
+	public Bolt(String nev, String cim, String tulajdonos, Tej[] tejpult) {
+		super();
+		this.nev = nev;
+		this.cim = cim;
+		this.tulajdonos = tulajdonos;
+		this.tejpult = tejpult;
+	}
+
+	public String getNev() {
+		return nev;
+	}
+
+	public String getCim() {
+		return cim;
+	}
+
+	public String getTulajdonos() {
+		return tulajdonos;
+	}
+
+	public boolean vanMegTej() {
+		if (tejpult.length > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public Tej vasarolTej(Tej m){
+		return m;
+	}
+
+}
